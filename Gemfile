6db2486cba52108af12f76be26049ca2b993aa7d
Vincent Hamonic <v.hamonic@gmail.com>
2016-08-10 15:04:24 +0200

    add routes + seed db

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'faker'
 
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Restaurant.create(name:Faker::Company.name, address:Faker::Address.street_address, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, phone_number: Faker::PhoneNumber.phone_number)
+end
